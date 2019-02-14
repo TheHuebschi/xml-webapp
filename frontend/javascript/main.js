5c1c4f8b9d71b05b27baf5b05e4f30c5ac36aa4f
Claudio Hübscher <claudio.huebscher@hotmail.com>
2019-02-14 18:24:18 +0100

    First draft of Frontend

---
+++ frontend/javascript/main.js
@@ -0,0 +1,68 @@
+function validateForm() {
+  var titleInput = document.forms["contactForm"]["titleInput"].value;
+  var nameInput = document.forms["contactForm"]["nameInput"].value;
+  var prenameInput = document.forms["contactForm"]["prenameInput"].value;
+  var emailInput = document.forms["contactForm"]["emailInput"].value;
+  if (titleInput == "Select") {
+    swal({
+      title: "Fehler!",
+      text: "Bitte gib mir deine Anrede an!",
+      icon: "error",
+      button: "Jetzt fixen!",
+    });
+  }
+  else {
+    $.ajax({
+      url: '/php/serversideProcessing.php',
+      type: 'POST',
+      data: {
+          'title': titleInput,
+          'name': nameInput,
+          'prename': prenameInput,
+          'mail': emailInput
+      },
+      success: function(data) {
+        message.innerHTML = data;
+      },
+      error: function(error) {
+        message.innerHTML = "Etwas ist schief gelaufen!";
+      }
+    });
+  }
+  return false;
+}
+
+function collapseNavigation() {
+    var x = document.getElementById("myTopnav");
+    if (x.className == "topnav") {
+        x.className += " responsive";
+    }
+    else {
+        x.className = "topnav";
+    }
+}
+
+function changePageContent(page) {
+  $.ajax({
+    url: 'html/' + page + '.html',
+    success: function(data) {
+      $('#content').html(data);
+    }
+  });
+  console.log("Loaded page content " + page);
+}
+
+function init() {
+  console.log("init");
+  var hash = window.location.hash;
+  if (hash) {
+    changePageContent(hash.replace("#", ""));
+  }
+  else {
+    changePageContent('home');
+  }
+}
+
+$(document).ready(
+	init()
+)
