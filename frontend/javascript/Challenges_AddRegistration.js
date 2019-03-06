04417b33b38c2ebd865515178ebed7dcfa1b9f9b
´Joun Chamas <joun.chamas@stud.hslu.ch>
2019-03-06 21:39:43 +0100

    new PHP and JS

---
+++ frontend/javascript/Challenges_AddRegistration.js
@@ -0,0 +1,93 @@
+window.onload = function() {
+    if(typeof getUrlVars()["id"] != 'undefined'){
+        prepareRequest();
+    }
+};
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+function prepareRequest(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            loadchallenge(xhttp.responseXML);
+        }
+    };
+    xhttp.open("GET", "../../database/challenges.xml", true);
+    xhttp.send(); 
+}
+
+function loadchallenge(xml) {
+    document.getElementById("TitleInput").value = getElementByXPath(xml, "title");
+    document.getElementById("descriptionInput").value = getElementByXPath(xml, "description");
+    document.getElementById("priceInput").value = getElementByXPath(xml, "price");
+    document.getElementById("companyId").value = getElementByXPath(xml, "companyId");
+    
+}
+
+function getElementByXPath(xml, elementName){
+    var xhttp = new XMLHttpRequest();
+    var result = "";
+    path = "/challenges/challenge[@id='" + getUrlVars()["id"] +"']/" + elementName
+    if (xml.evaluate) {
+        var nodes = xml.evaluate(path, xml, null, XPathResult.ANY_TYPE, null);
+        result = nodes.iterateNext().childNodes[0].nodeValue;
+    // Code For Internet Explorer
+    } else if (window.ActiveXObject || xhttp.responseType == "msxml-document") {
+        xml.setProperty("SelectionLanguage", "XPath");
+        result = xml.selectNodes(path).nodes[0].childNodes[0].nodeValue;
+    }
+    return result;
+}
+
+function editChallenge() {
+    var idInput = getUrlVars()["id"];
+    var companyId = document.forms["newChallengeform"]["companyId"].value;
+    var titleInput = document.forms["newChallengeform"]["TitleInput"].value;
+    var priceInput = document.forms["newChallengeform"]["priceInput"].value;
+    var descriptionInput = document.forms["newChallengeform"]["descriptionInput"].value;
+    $.ajax({
+    url: '../php/Challenges_AddRegistration.php',
+    type: 'POST',
+    data: {
+        'id': idInput,
+        'title': titleInput,
+        'price': priceInput,
+        'description': descriptionInput,
+        'companyId': companyId
+    },
+    success: function() {
+        success();
+    },
+    error: function() {
+        error();
+    }
+    });
+    return false;
+  }
+
+  function success() {
+    swal({
+        title: "Geschafft!",
+        text: "Deine Challenge wurde erfolgreich hinzugefügt!",
+        icon: "success",
+        button: "Weiter entdecken!",
+    }).then(() => {
+        window.location.href = "challenges.xhtml";
+    }); 
+  }
+
+  function error() {
+    swal({
+        title: "Oh oh!",
+        text: "Die Eingaben konnten nicht erfolgreich validiert werden!",
+        icon: "error",
+        button: "Jetzt fixen!",
+      });
+  }
---
+++ frontend/php/Challenges_AddRegistration.php
@@ -0,0 +1,88 @@
+<?php 
+    $id = htmlspecialchars($_POST["id"]);
+    $companyId = htmlspecialchars($_POST["companyId"]);
+    $title = htmlspecialchars($_POST["title"]);
+    $price = htmlspecialchars($_POST["price"]);
+    $description = htmlspecialchars($_POST["description"]);
+    
+    
+    $xml = simplexml_load_file('../../database/challenges.xml');
+
+    if (empty($id)) {
+        $id = getNextFreeId($xml);
+        addChallenge($xml, $id,$companyId, $title, $price, $description);
+    }
+    else {
+        editChallenge($xml, $id, $companyId, $title, $price, $description);
+    }
+    
+    persistXML('../../database/challenges_temp.xml', $xml);
+    
+    if (validateXML()) {
+        replaceXML();
+        removeTempXML();
+        http_response_code(200);
+    } else {
+        removeTempXML();
+        http_response_code(500);
+    }
+
+    function getNextFreeId($xml) {
+        $xPathQuery = '//@id';
+        $results = $xml->xpath($xPathQuery);
+        rsort($results);
+        
+        return $results[count($results)-1] + 1;
+    }
+
+    function addChallenge($xml, $id,$companyId, $title, $price, $description) {
+        $challenge = $xml->addChild('challenge');
+        $challenge->addAttribute('id', $id);
+        $challenge->addAttribute('companyId', $companyId);
+        $challenge->addChild('title', $title);
+        $challenge->addChild('description', $description);
+        $challenge->addChild('price', $price);
+        
+    }
+
+    function editChallenge($xml, $id, $companyId, $title, $price, $description) {
+        $xPathQuery = '//challenge[@id="' . "$id" . '" @companyId="' . "$companyId" . '"]';
+        $challenge = $xml->xpath($xPathQuery)[0];
+
+        $challenge->title = $title;
+        $challenge->price = $price;
+        $challenge->description = $description;
+      //  $challenge->companyId = $companyId;
+    }
+
+    function persistXML($path, $xml) {
+        file_put_contents($path, $xml->asXML());
+    }
+
+    function validateXML() {
+        $data = file_get_contents('../../database/challenges_temp.xml');
+        $xml = new DOMDocument();
+        $xml->loadXML($data);
+
+        libxml_use_internal_errors(true);
+        
+        $xsd = '../../database/challenges.xsd';
+        $result = $xml->schemaValidate($xsd);
+        
+        if (!$result) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    function replaceXML() {
+        unlink('../../database/challenges.xml');
+        rename('../../database/challenges_temp.xml', '../../database/challenges.xml');
+    }
+
+    function removeTempXML() {
+        unlink('../../database/challenges_temp.xml');
+    }
+?>
